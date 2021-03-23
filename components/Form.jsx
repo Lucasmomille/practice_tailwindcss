7cc319deb1cf3589124b6fb704db37ce9a0a3ade
Lucas <lucas.moquet95@gmail.com>
2021-03-23 09:48:27 +0100

    package.json ok heroku

--- components/Form.jsx
+++ components/Form.jsx
@@ -5,19 +5,34 @@ import {useRef, useState} from "react"
     name: String;
     email: String;
 } */
-  
+
+const wait = function (duration = 1000) {
+    return new Promise((resolve) => {
+        window.setTimeout(resolve, duration)
+    })
+}
+
 export default function Form() {
-    const {register} = useForm();
+    const {register, handleSubmit, formState, errors} = useForm();
+
+    const onSubmit = async data => {
+        await wait(2000)
+        console.log(data);
+    }
+
+    console.log(formState);
 
     return (
-        <form action="submit" className="flex flex-row">
+        <form action="submit" className="flex flex-row" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col">
             <label htmlFor="username">Prénom</label>
-            <input type="text" id="username" name="username" className="" />
+            <input type="text" id="username" name="username" className="" ref={register({required: true})} />
+            {errors.username && <span className="text-red-500 text-xs">Vous devez renseigner ce champ</span>}
         </div>
         <div className="flex flex-col">
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="useremail"/>
+            <input type="text" id="email" name="useremail" ref={register({required: true})}/>
+            {errors.username && <span className="text-red-500 text-xs">Vous devez renseigner ce champ</span>}
         </div>
         <button className="rounded-full w-20 h-20 bg-brown-btn p-2 text-white font-bold text-3xl">&rsaquo;</button>
         </form>
